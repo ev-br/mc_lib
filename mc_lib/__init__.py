fd9860a679b5adcc9d5bc98cfbf1f6f41909d8c1
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2021-06-29 18:16:52 +0300

    MAINT: bump version to 0.2

--- mc_lib/__init__.py
+++ mc_lib/__init__.py
@@ -7,7 +7,7 @@ from mc_lib.lattice import tabulate_neighbors
 from mc_lib.rndm cimport RndmWrapper
 """
 
-__version__ = "0.1"
+__version__ = "0.2"
 
 # This import will check whether the cython sources have been built,
 # and if not will raise a useful error.
