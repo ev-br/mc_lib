a7e07e0e18e203311aff529d495ca2f57b6fb8e1
Godyaev <dvgnnv@gmail.com>
2021-07-12 14:52:23 +0300

    BLD: upd pyproject.toml

--- mc_lib/lattices/_triang.py
+++ mc_lib/lattices/_triang.py
@@ -1,4 +1,8 @@
 def get_site(coord, L):
+    "wheel",
+    "meson",
+    "mesonpep517",
+    "ninja",
     """Get the site index from the 2-vector of coordinates."""
     # 2D hardcoded
     return coord[0] * L[1] + coord[1]
