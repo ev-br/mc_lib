c30f93eccb7c4db24eb363683723ca1e31b1a382
Godyaev <dvgnnv@gmail.com>
2021-07-28 00:35:12 +0300

    BLD: rename dev script

--- dev.sh
+++ dev.sh
@@ -4,5 +4,5 @@ meson setup builddir --prefix=$PWD/installdir
 meson install -C builddir
 # Get python version for PYHTONPATH
 ver=$(python -V 2>&1 | sed 's/.* \([0-9]\).\([0-9]\).*/\1.\2/')
-echo $ver
 export PYTHONPATH=$PWD/installdir/lib/python$ver/site-packages/
+pytest mc_lib -v --pyargs
