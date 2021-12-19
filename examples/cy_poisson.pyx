912b0e9e093daa604188b3a09287bb3021ee5516
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2021-12-20 00:34:22 +0300

    DOC: poisson example: document the woodoo with the NumPy random C API

--- examples/cy_poisson.pyx
+++ examples/cy_poisson.pyx
@@ -1,11 +1,26 @@
+"""
+This is a simple example of using NumPy random C API to generate non-uniform
+variates with a RndmWrapper instance.
+
+The NumPy random C API has generator functions which receive a `bitgen_t*` struct,
+which a RndmWrapper instance wraps.
+
+The main woodoo is linking with the right numpy libraries (`npyrandom` and `npymath`),
+which happens in the `meson.build` file.
+
+Refences: 
+
+[1] https://numpy.org/devdocs/reference/random/c-api.html
+[2] https://numpy.org/devdocs/reference/random/extending.html
+"""
+
 import numpy as np
-cimport numpy as np
 from mc_lib.rndm cimport RndmWrapper
 from numpy.random.c_distributions cimport random_poisson
 
 
 def generate(n_samples, lam=2):
-    """Generate n_samples from the Poisson distribution with given lambda.
+    """Generate n_samples from the Poisson distribution with a given lambda.
     """
     cdef RndmWrapper rndm = RndmWrapper((1234, 0))
     cdef long[::1] vals = np.empty((n_samples,), dtype=int)
